c1c752b9b8ce081ebd569ca2e8d5ca550a466f86
Julia Gao Miller <julia.gao.miller@gmail.com>
2017-12-30 13:29:29 -0800

    switching layout around as you navigate between views: multiple router views

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,19 @@ import Home from './components/Home.vue';
 import UserStart from './components/user/UserStart.vue';
 import UserEdit from './components/user/UserEdit.vue';
 import UserDetail from './components/user/UserDetail.vue';
+import Header from './components/Header.vue';
 
 export const routes = [
-  { path: '', component: Home, name: 'home' },
-  { path: '/user', component: User, children: [
+  { path: '', name: 'home', components: {
+    default: Home,
+    'header-top': Header,
+    }
+  },
+  { path: '/user', components: {
+    default: User,
+    'header-bottom': Header,
+    },
+    children: [
     // these are nested routes
     { path: '', component: UserStart},
     { path: ':id', component: UserDetail},
