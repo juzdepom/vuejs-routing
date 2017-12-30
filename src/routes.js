a86b1b215fdf1e61537ee16b78dd9cf5b7c0cdab
Julia Gao Miller <julia.gao.miller@gmail.com>
2017-12-30 14:17:38 -0800

    this is how you redirect routes

--- src/routes.js
+++ src/routes.js
@@ -21,4 +21,5 @@ export const routes = [
     { path: ':id', component: UserDetail},
     { path: ':id/edit', component: UserEdit, name: 'userEdit'},
   ]},
-]
+  { path: '/redirect-me', redirect: {name: 'home'} }
+];
