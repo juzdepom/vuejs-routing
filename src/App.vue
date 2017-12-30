18152581af455f93e7fb7556d3b18932f6d3c4f8
Julia Gao Miller <julia.gao.miller@gmail.com>
2017-12-30 11:45:03 -0800

    navigating with router links

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="container">
         <div class="row">
+
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Routing</h1>
                 <hr>
+                <app-header></app-header>
                 <!-- this element ships with the vue router -->
                 <router-view></router-view>
             </div>
@@ -12,7 +14,12 @@
 </template>
 
 <script>
+    import Header from './components/Header.vue'
+
     export default {
+      components: {
+        appHeader: Header,
+      }
     }
 </script>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul class="nav nav-pills">
+    <li class="nav-item">
+      <!-- using router-link instead of the normal anchor tag stops the page from reloading -->
+      <router-link to="/">Home</router-link>
+    </li>
+    <li class="nav-item">
+      <router-link to="/user">User</router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
