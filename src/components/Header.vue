74bede9d3a04377d213732bb0a8654e776b1bc1f
Julia Gao Miller <julia.gao.miller@gmail.com>
2017-12-30 12:00:44 -0800

    setting up, fetching and using route parameters.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
   <ul class="nav nav-pills">
       <!-- using router-link instead of the normal anchor tag stops the page from reloading -->
       <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
-      <router-link to="/user" tag="li" active-class="active"><a>User</a></router-link>
+      <router-link to="/user/10" tag="li" active-class="active"><a>User</a></router-link>
   </ul>
 </template>
 
