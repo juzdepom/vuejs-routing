1705741499c78838fb63f9398dddec35c76ebcdd
Julia Gao Miller <julia.gao.miller@gmail.com>
2017-12-30 11:51:03 -0800

    styling active links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,8 @@
 <template>
   <ul class="nav nav-pills">
-    <li class="nav-item">
       <!-- using router-link instead of the normal anchor tag stops the page from reloading -->
-      <router-link to="/">Home</router-link>
-    </li>
-    <li class="nav-item">
-      <router-link to="/user">User</router-link>
-    </li>
+      <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
+      <router-link to="/user" tag="li" active-class="active"><a>User</a></router-link>
   </ul>
 </template>
 
