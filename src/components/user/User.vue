0ce3d7651495f22b79f0d6213ec0b513078c9752
Julia Gao Miller <julia.gao.miller@gmail.com>
2017-12-30 11:55:51 -0800

    navigating from code: added Go To Home button on User page

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,4 +1,18 @@
 <template>
+  <div class="">
     <h1>The User Page</h1>
-
+    <hr>
+    <button @click="navigateToHome" class="btn btn-primary">Go To Home</button>
+  </div>
 </template>
+
+<script>
+  export default {
+    methods: {
+      navigateToHome(){
+        this.$router.push('/');
+      }
+    }
+  }
+
+</script>
